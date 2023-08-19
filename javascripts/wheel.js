5349a2697e498a55d8a8370fd724878cd1058a75
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2023-08-19 08:20:55 -0700

    Update wheel.js

--- javascripts/wheel.js
+++ javascripts/wheel.js
@@ -30,7 +30,7 @@ var students = [
   { name: 'Spawn 1 druid' },
   { name: 'Spawn 20 turtles' },
   { name: 'Blessing' }
-]
+];
 
 var shuffle = function (o) {
   for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
